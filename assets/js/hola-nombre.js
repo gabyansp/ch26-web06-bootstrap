a5ac257a87807cb00cabd3a1d8fc06d5994a489b
Gabi Peña <gabriela_salazar03@outlook.es>
2023-04-24 20:49:10 -0600

    Se agrega pag. cambiar nombre

---
+++ assets/js/hola-nombre.js
@@ -0,0 +1,7 @@
+const boton = document.getElementById('boton');
+const saludo = document.getElementById('saludo');
+
+boton.addEventListener('click', () => {
+  const nombre = prompt('¿Cuál es tu nombre?');
+  saludo.textContent = `Hola ${nombre}`;
+});
